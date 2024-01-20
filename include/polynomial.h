78d7959690d1e01fe080d1e88094e658d5f54a63
boltfang <vincent.dev.dong@gmail.com>
2024-01-20 00:13:42 -0500

    Polynomial Operations

--- include/polynomial.h
+++ include/polynomial.h
@@ -14,6 +14,7 @@ typedef struct {
 
 void initializePolynomial(Polynomial *polynomial, int degree);
 void setCoefficient(Polynomial *polynomial, int exponent, double coefficient);
+void updateCoefficient(Polynomial *polynomial, int exponent, double coefficient);
 void copyPolynomial(const Polynomial *source, Polynomial *destination);
 Polynomial addPolynomials(const Polynomial *addened_1, const Polynomial *addend_2);
 Polynomial subtractPolynomials(const Polynomial *minuend, const Polynomial *subtrahend);
